2d62d239cae8012703038e9ea5f4ee15e80d4851
WhiskeyRomeoLima <wrlucas67@gmail.com>
2022-09-06 13:15:56 -0500

    removed remarks from ContactPicker

--- src/components/appointments/contactPicker.jsx
+++ src/components/appointments/contactPicker.jsx
@@ -1,25 +1,5 @@
 import React from 'react';
 
-// const ContactPicker = () => ({ name, onChange, contacts, getContactNames }) => {
-//   const names = getContactNames()
-//   console.log('contact names: ',names);
-
-//   return (
-//     <select name={name} onChange={onChange}>
-//       <option value={""} key={-1} selected="selected">
-//         No Contact Selected
-//       </option>
-//       {names.map((name) => {
-//         return (
-//           <option value={name} key={name}>
-//             {name}
-//           </option>
-//         );
-//       })}
-//     </select>
-//   );
-// };
-
 const ContactPicker = ({ contact, contacts, onChange, getContactNames }) => {
   const names = getContactNames();
   return (
